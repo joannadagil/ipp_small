23c30ed307d38992dff77589e71caf121fa954df
JoannaDagil <jd417531@students.mimuw.edu.pl>
2021-08-10 22:37:18 +0200

    test.sh pomocy

--- test.sh
+++ test.sh
@@ -12,13 +12,13 @@ do
     valgrind --error-exitcode=15 --leak-check=full --show-leak-kinds=all --errors-for-leak-kinds=all -q\
 	  ${PROG} <$f >${f%.in}2.out 2>${f%.in}2.err
     
-    if [diff ${f%.in}2.out ${f%.in}.out || diff ${f%.in}2.err ${f%.in}.err]
+    if diff ${f%.in}2.out ${f%.in}.out || diff ${f%.in}2.err ${f%.in}.err
     then
       echo -e "$TEST - ok"
     else
       echo -e "$TEST - wrong"
     fi
-    
+
     rm ${f%.in}2.out ${f%.in}2.err
 done
 
