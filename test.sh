95aee146d3746081e745d9c31d32907a71e20a6b
JoannaDagil <jd417531@students.mimuw.edu.pl>
2021-08-12 11:19:00 +0200

    testuj again

--- test.sh
+++ test.sh
@@ -3,19 +3,19 @@
 PROG=$1
 DIR=$2
 
-for f in ${DIR}/*.in
+for TEST in $DIR/*.in
 do
-    TEST=${f%.in}
+    #TEST=${f%.in}
 
     #valgrind --error-exitcode=15 --leak-check=full --show-leak-kinds=all --errors-for-leak-kinds=all -q\
-	  ./${PROG} <$f >${f%.in}2.out 2>${f%.in}2.err
+	  ./${PROG} <$TEST >${TEST%.in}2.out 2>${TEST%.in}2.err
     
-    if diff ${f%.in}2.out ${f%.in}.out || diff ${f%.in}2.err ${f%.in}.err
+    if [diff ${TEST%.in}2.out ${TEST%.in}.out || diff ${TEST%.in}2.err ${TEST%.in}.err]
     then
       echo -e "$TEST - ok"
     else
       echo -e "$TEST - wrong"
     fi
 
-    rm ${f%.in}2.out ${f%.in}2.err
+    rm ${TEST%.in}2.out ${TEST%.in}2.err
 done
