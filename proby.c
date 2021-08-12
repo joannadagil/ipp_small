cb2bf29f4b0646109ee3a7f05dc885ceedc489f3
JoannaDagil <jd417531@students.mimuw.edu.pl>
2021-08-12 17:30:42 +0200

    testuj test

--- proby.c
+++ proby.c
@@ -6,5 +6,22 @@
 int main(){
 
     printf("mama\n");
+
+    size_t size = 100;
+	char *row;
+    //Tree data;
+    //data = NULL;
+    //List_list all_lines;
+    //all_lines = NULL;
+	int read;
+    int line_no = 1;
+    long double *n;
+    char **nan;
+    int n_size, nan_size;
+
+    while ((read = getline(&row, &size, stdin)) != -1){
+        line_no++;
+	}
+
     return 0;
 }
