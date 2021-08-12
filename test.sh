838c9dce28f84324e479375b0ab2fca06543bc1a
JoannaDagil <jd417531@students.mimuw.edu.pl>
2021-08-12 12:08:40 +0200

    testuj again3

--- test.sh
+++ test.sh
@@ -10,10 +10,10 @@ do
     #valgrind --error-exitcode=15 --leak-check=full --show-leak-kinds=all --errors-for-leak-kinds=all -q\
 	  ./${PROG} < ${TEST} > ${TEST%.in}2.out 2> ${TEST%.in}2.err
 
-    DIFF1 = $(diff ${TEST%.in}2.out ${TEST%.in}.out)
-    DIFF2 = $(diff ${TEST%.in}2.err ${TEST%.in}.err)
+    DIFF1=$(diff ${TEST%.in}2.out ${TEST%.in}.out)
+    DIFF2=$(diff ${TEST%.in}2.err ${TEST%.in}.err)
     
-    if [ DIFF1 == "" || DIFF2 == "" ];
+    if [[ $DIFF1 == "" || $DIFF2 == "" ]];
     then
       echo -e "$TEST - ok"
     else
