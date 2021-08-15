c13b072a8b790dddb75c2f1d18cbd9107912a254
JoannaDagil <jd417531@students.mimuw.edu.pl>
2021-08-15 19:00:46 +0200

    testuj test

--- main.c
+++ main.c
@@ -15,9 +15,7 @@ int main(){
 	size_t size = STARTVALUE;
 	char *row = NULL;
     int read;
-
     int line_no = 1;
-
 
     Tree data;
     data = NULL;
@@ -28,16 +26,16 @@ int main(){
     int n_size, nan_size;
 
 	while ((read = getline(&row, &size, stdin)) != -1){
-        //if (errno == ENOMEM) exit(1);
-        //int valid = process_line(row, &n, &n_size, &nan, &nan_size);
-        //if(valid > 0)
-        //    data = add_line(n, n_size, nan, line_no, data, &all_lines);
-        //if(valid < 0)
-        //    fprintf(stderr, "ERROR %d\n", line_no);
+        if (errno == ENOMEM) exit(1);
+        int valid = process_line(row, &n, &n_size, &nan, &nan_size);
+        if(valid > 0)
+            data = add_line(n, n_size, nan, line_no, data, &all_lines);
+        if(valid < 0)
+            fprintf(stderr, "ERROR %d\n", line_no);
         line_no++;
 	}
-    //delete_tree(data);
-    //print_delete_list_list(all_lines);
+    delete_tree(data);
+    print_delete_list_list(all_lines);
 
     return 0;
 }
