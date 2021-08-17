eef615764daf013fe06eced63f20d658c87d0a81
JoannaDagil <jd417531@students.mimuw.edu.pl>
2021-08-17 13:29:27 +0200

    test

--- test.sh
+++ test.sh
@@ -7,10 +7,13 @@ for TEST in $DIR/*.in;
 do
     #TEST=${f%.in}
 
-    valgrind --error-exitcode=15 --leak-check=full --show-leak-kinds=all --errors-for-leak-kinds=all ./${PROG} < ${TEST} > ${TEST%.in}2.out 2> ${TEST%.in}2.err
+    valgrind --error-exitcode=15 --leak-check=full --show-leak-kinds=all --errors-for-leak-kinds=all ./${PROG} < ${TEST} > test.out 2> test.err
 
-    DIFF1=$(diff ${TEST%.in}2.out ${TEST%.in}.out)
-    DIFF2=$(diff ${TEST%.in}2.err ${TEST%.in}.err)
+    DIFF1=$(diff test.out ${TEST%.in}.out)
+    DIFF2=$(diff test.err ${TEST%.in}.err)
+
+    echo -e "$DIFF1"
+    echo -e "$DIFF2"
     
     if [[ $DIFF1 == "" || $DIFF2 == "" ]];
     then
