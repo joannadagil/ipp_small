a1cf131b3091181f78371b4cbaaaeb01932aa9e5
JoannaDagil <jd417531@students.mimuw.edu.pl>
2021-08-23 20:23:50 +0200

    nulle w srodku wierszy

--- main.c
+++ main.c
@@ -25,7 +25,7 @@ int main(){
     int *n_size; int *nan_size; //gwaizki
     n_size = malloc(sizeof(int)); nan_size = malloc(sizeof(int));
 
-	while ((read = getline(&row, &size, stdin)) >= 0){
+	while ((read = getline(&row, &size, stdin)) != -1){
         if (errno == ENOMEM) exit(1);
         int valid = process_line(row, &n, n_size, &nan, nan_size, read); //& z sizów
         if(valid > 0)
