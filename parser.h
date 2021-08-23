f9a488c93e77aa4f034d32a5f9a5c7292edb8b72
JoannaDagil <jd417531@students.mimuw.edu.pl>
2021-08-23 20:07:27 +0200

    nulle w srodku wierszy

--- parser.h
+++ parser.h
@@ -1,6 +1,6 @@
 #ifndef PARSER_H
 #define PARSER_H
 
-extern int process_line(char* line, long double **n, int *n_i, char ***nan, int *nan_i, size_t read);
+extern int process_line(char* line, long double **n, int *n_i, char ***nan, int *nan_i, int read);
 
 #endif /* PARSER_H */
